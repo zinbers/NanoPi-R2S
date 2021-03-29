befb008e66552854737058f056ae2d2b59926ebe
MedyMa <75345465+MedyMa@users.noreply.github.com>
2021-03-29 19:37:55 +0800

    Update lean-openwrt.sh

--- scripts/lean-openwrt.sh
+++ scripts/lean-openwrt.sh
@@ -75,8 +75,7 @@ rm -rf ../lean/luci-theme-infinityfreedom
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom
 
 # Add tmate
-#git clone --depth=1 https://github.com/immortalwrt/openwrt-tmate
-svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-19.07/package/ctcgfw/tmate
+git clone --depth=1 https://github.com/immortalwrt/openwrt-tmate
 
 # Add subconverter
 git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter

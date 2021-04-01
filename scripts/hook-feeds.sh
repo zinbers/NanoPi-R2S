7a02102c6e0e48ab6dab3d7afe068203cf6c09c6
MedyMa <75345465+MedyMa@users.noreply.github.com>
2021-04-01 08:30:28 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -12,7 +12,8 @@ pushd customfeeds
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-gowebdav luci/applications/luci-app-gowebdav
 svn co https://github.com/immortalwrt/packages/trunk/net/gowebdav packages/net/gowebdav
 
-# Add smartdns
+# Add smartdns&luci-app-smartdns
+svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-smartdns luci/applications/luci-app-smartdns
 svn co https://github.com/immortalwrt/packages/trunk/net/smartdns packages/net/smartdns
 
 # Add tmate

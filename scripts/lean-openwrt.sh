e468167825dbbacc6f247c1a4caf515293360de4
MedyMa <75345465+MedyMa@users.noreply.github.com>
2021-04-04 18:09:03 +0800

    Update lean-openwrt.sh

--- scripts/lean-openwrt.sh
+++ scripts/lean-openwrt.sh
@@ -34,6 +34,12 @@ git clone --depth=1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk
 # Add luci-proto-minieap
 git clone --depth=1 https://github.com/ysc3839/luci-proto-minieap
 
+# Add luci-app-netdata
+rm -rf ../lean/luci-app-netdata
+svn co https://github.com/281677160/openwrt-package/trunk/luci-app-netdata
+rm -rf ../../customfeeds/packages/admin/netdata
+svn co https://github.com/281677160/openwrt-package/trunk/netdata
+
 # Add luci-app-smartdns & smartdns
 svn co https://github.com/281677160/openwrt-package/trunk/luci-app-smartdns
 svn co https://github.com/281677160/openwrt-package/trunk/smartdns

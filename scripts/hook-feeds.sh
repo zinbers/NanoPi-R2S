7057541a53db2b9c85ea66e4289e730f52f55dde
MedyMa <75345465+MedyMa@users.noreply.github.com>
2021-04-11 08:53:31 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -7,40 +7,30 @@
 
 # Svn checkout packages from immortalwrt's repository
 pushd customfeeds
-mkdir temp
-git clone --depth=1 https://github.com/immortalwrt/packages -b openwrt-18.06 temp/packages
-git clone --depth=1 https://github.com/immortalwrt/luci -b openwrt-18.06 temp/luci
 
 # Add luci-app-adguardhome
-cp -r temp/luci/applications/luci-app-adguardhome luci/applications/luci-app-adguardhome
-cp -r temp/packages/net/adguardhome packages/net/adguardhome
-cp -r temp/packages/lang/node-yarn packages/lang/node-yarn
-cp -r temp/packages/devel/packr packages/devel/packr
+svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06/applications/luci-app-adguardhome luci/applications/luci-app-adguardhome
+svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/net/adguardhome packages/net/adguardhome
+svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/lang/node-yarn packages/lang/node-yarn
+svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/devel/packr packages/devel/packr
 
 # Add luci-app-gowebdav
-cp -r temp/luci/applications/luci-app-gowebdav luci/applications/luci-app-gowebdav
-cp -r temp/packages/net/gowebdav packages/net/gowebdav
-
-# Add netdata
-rm -rf packages/admin/netdata
-rm -rf ../package/lean/luci-app-netdata
-cp -r temp/luci/applications/luci-app-netdata luci/applications/luci-app-netdata
-cp -r temp/packages/admin/netdata packages/admin/netdata
+svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06/applications/luci-app-gowebdav luci/applications/luci-app-gowebdav
+svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/net/gowebdav packages/net/gowebdav
 
 # Add luci-app-smartdns
-cp -r temp/luci/applications/luci-app-smartdns luci/applications/luci-app-smartdns
-cp -r temp/packages/net/smartdns packages/net/smartdns
+svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06/applications/luci-app-smartdns luci/applications/luci-app-smartdns
+svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/net/smartdns packages/net/smartdns
 
 # Add tmate
-cp -r temp/packages/net/tmate packages/net/tmate
-cp -r temp/packages/libs/msgpack-c packages/libs/msgpack-c
+svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/net/tmate packages/net/tmate
+svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/libs/msgpack-c packages/libs/msgpack-c
 
 # Add gotop
-cp -r temp/packages/admin/gotop packages/admin/gotop
+svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/admin/gotop packages/admin/gotop
 
 # Add minieap
-cp -r temp/packages/net/minieap packages/net/minieap
-rm -rf temp
+svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/net/minieap packages/net/minieap
 popd
 
 # Set to local feeds

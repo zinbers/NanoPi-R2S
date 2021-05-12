8c09f190feab9d27f61a4f8efa0fb1d54fef6437
MedyMa <75345465+MedyMa@users.noreply.github.com>
2021-05-12 13:55:01 +0800

    Update lean-openwrt.sh

--- scripts/lean-openwrt.sh
+++ scripts/lean-openwrt.sh
@@ -11,6 +11,7 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # Add luci-app-ssr-plus
 pushd package/lean
 git clone --depth=1 https://github.com/fw876/helloworld
+svn co https://github.com/immortalwrt/immortalwrt/trunk/package/lean/shadowsocksr-libev
 popd
 
 # Clone community packages to package/community
